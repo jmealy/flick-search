7929306209aa307ce6126d32b2dfb49ce611a066
James <jamesmealy1@gmail.com>
2019-11-08 14:16:58 +0100

    only api key in config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,3 @@
 export default {
-  flickrUrl: 'https://www.flickr.com/services/rest/?method=flickr.photos.search',
-  apiKey: '0108b623dbb81829b5f013074dee22bb',
-  maxImages: 100,
-  imagesPerPage: 10
+  apiKey: '0108b623dbb81829b5f013074dee22bb'
 }
