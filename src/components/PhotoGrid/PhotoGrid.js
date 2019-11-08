41207ef85521681816f7fe9f92bb3e7ff1007dce
James <jamesmealy1@gmail.com>
2019-11-08 12:48:32 +0100

    clean up

--- src/components/PhotoGrid/PhotoGrid.js
+++ src/components/PhotoGrid/PhotoGrid.js
@@ -7,40 +7,21 @@ import './PhotoGrid.css'
 const PhotoGrid = (props) => {
   const pageSize = 10;
   const [pageNum, setPageNum] = useState(1);
-  const [imagesLoaded, setImagesLoaded] = useState(0);
-  // reset to first page when new search term is entered.
   useEffect(props => setPageNum(1), [props.images]);
-  
+
   if (!props.images) return null;
 
   const loadMore = () => {
     setPageNum(pageNum + 1);
   }
 
-  const onImageLoad = () => {
-    // setImagesLoaded(count => count + 1);
-    const newCount = imagesLoaded + 1;
-    setImagesLoaded(newCount);
-
-    if (newCount === pageNum * pageSize) {
-
-    }
-  }
-
   return (
     <div className="photoGrid">
       <div className="photos">
         {
-          true ? (
-            props.images.slice(0, pageNum * pageSize).map(photo => (
-              <img onLoad={onImageLoad} src={photo} />
-            ))
-          ) : null
-        }
-        {
-          false ? (
-            <p> Loading...</p>
-          ) : null
+          props.images.slice(0, pageNum * pageSize).map(photo => (
+            <img src={photo} />
+          ))
         }
       </div>
       {
